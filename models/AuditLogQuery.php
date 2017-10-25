faf0d4118c8884ec71030c233a44fd54c8338be8
Stavros Papadakis <spapad@gmail.com>
2017-10-25 09:37:49 +0300

    Add logins and submittions lookup methods in active query

--- models/AuditLogQuery.php
+++ models/AuditLogQuery.php
@@ -13,6 +13,30 @@ class AuditLogQuery extends \yii\db\ActiveQuery
         return $this->andWhere(['like', 'prefix', "[" . intval($id) . "]"]);
     }
 
+    public function level($level)
+    {
+        return $this->andWhere(['level' => $level]);
+    }
+
+    public function category($category)
+    {
+        return $this->andWhere(['like', 'UPPER({{category}})', strtoupper($category)]);
+    }
+
+    public function logins()
+    {
+        return $this->level(\yii\log\Logger::LEVEL_INFO)
+                ->category('user.login')
+                ->orderBy(['id' => SORT_DESC]);
+    }
+
+    public function applicationSubmits()
+    {
+        return $this->level(\yii\log\Logger::LEVEL_INFO)
+                ->category('user.application.submit')
+                ->orderBy(['id' => SORT_DESC]);
+    }
+
     /**
      * @inheritdoc
      * @return AuditLog[]|array
