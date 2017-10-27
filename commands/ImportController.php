f6bde918be811591a75154ec1af0ae48dfbc03aa
Stavros Papadakis <spapad@gmail.com>
2017-10-27 09:58:24 +0300

    Import and load data

--- commands/ImportController.php
+++ commands/ImportController.php
@@ -16,7 +16,7 @@ use League\Csv\Statement;
 class ImportController extends Controller
 {
 
-    public $defaultAction = 'parse';
+    public $defaultAction = 'load';
 
     /**
      * @var string If "yes" the command also invokes the necessary fixture load command
@@ -40,23 +40,39 @@ class ImportController extends Controller
 
     /**
      * @var string Numeric index of the fields to retrieve. In order:
+     * For the applicants: 
      * Prefecture preference, Lastname, Firstname, Fathername, Mothername, Phone, VAT, ID card, email
      */
-    public $fields = '1,2,3,4,5,6,9,11,12';
+    public $fields_applicants = '1,2,3,4,5,6,9,11,12';
+
+    /**
+     * @var string Numeric index of the fields to retrieve. In order:
+     * For the choices:
+     * Specialty, Position count, Position description, Prefecture (only the first letter in capital)
+     */
+    public $fields_choices = '0,1,2,3';
 
     /**
      * @var map associate ids to attributes
      */
     private $_field_mapping = [
-        'preferences',
-        'lastname',
-        'firstname',
-        'fathername',
-        'mothername',
-        'mobile',
-        'vat',
-        'identity',
-        'email'
+        'applicants' => [
+            'preferences',
+            'lastname',
+            'firstname',
+            'fathername',
+            'mothername',
+            'mobile',
+            'vat',
+            'identity',
+            'email'
+        ],
+        'choices' => [
+            'specialty',
+            'count',
+            'position',
+            'prefecture'
+        ]
     ];
 
     public function options($actionID)
@@ -71,52 +87,180 @@ class ImportController extends Controller
     }
 
     /**
-     * TODO
-     *
-     * @param string $text_to_crypt the text to be crypted.
+     * Check the data contained in a serialized data file and display on stdout.
+     * 
+     * @param string $serialized_file The file containing the serialized data
      */
-    public function actionParse($file)
+    public function actionShow($serialized_file)
+    {
+        $serialized_data = file_get_contents($serialized_file);
+        $data = unserialize($serialized_data);
+        echo print_r($data, true), PHP_EOL;
+        return Controller::EXIT_CODE_NORMAL;
+    }
+
+    /**
+     * Parse and generate serialized data files with the applicant and choices data included in CSV files.
+     *
+     * @param string $applicants_file the filename of the CSV containing the applicant information
+     * @param string $choices_file the filename of the CSV containing the choices information
+     */
+    public function actionParse($applicants_file, $choices_file)
+    {
+        $this->stdout("Initiating data parse and import file generation...\n", Console::BOLD);
+        $this->stdout("Getting choices data...\n", Console::BOLD);
+        $data1 = $this->getApplicantData($applicants_file);
+        if (!is_array($data1)) {
+            return $data1;
+        }
+
+        $this->stdout("Getting applicant data...\n", Console::BOLD);
+        $data2 = $this->getChoicesData($choices_file);
+        if (!is_array($data2)) {
+            return $data2;
+        }
+
+        $data = [
+            'applicant' => $data1['applicant'],
+            'specialty' => array_unique(array_merge($data1['specialty'], $data1['specialty'])),
+            'prefecture' => $this->getPrefecturesData(),
+            'choices' => $data2['choices'],
+            'prefecture_preference' => $data1['prefecture_preference']
+        ];
+
+        $filename = $this->getOutputFilename($applicants_file);
+        if (($saved = file_put_contents($filename, serialize($data))) === false) {
+            $this->stderr("Cannot save serialized data to file {$filename}\n", Console::FG_RED);
+            return Controller::EXIT_CODE_ERROR;
+        }
+
+        $this->stdout("Data parsed and saved to import file {$filename}\n", Console::BOLD);
+
+        if (strtoupper($this->runFixture) === 'YES') {
+            $this->stdout("Running fixture load command\n", Console::BOLD);
+            Yii::setAlias('@import-data-dir', Yii::getAlias('@app/commands/data-files'));
+            $run_fixture = \Yii::$app->runAction('fixture/load', ['Application', 'namespace' => 'app\tests\importFixtures']);
+        }
+
+        return Controller::EXIT_CODE_NORMAL;
+    }
+
+    /**
+     * Parse and generate serialized data files with the applicant data included in a CSV file.
+     *
+     * @param string $file the filename of the CSV containing the applicant information
+     */
+    public function actionParseApplicants($file)
     {
         $this->stdout("Initiating data parse and import file generation...\n", Console::BOLD);
 
+        $data = $this->getApplicantData($file);
+        if (!is_array($data)) {
+            return $data;
+        }
+
+        $filename = $this->getOutputFilename($file);
+        if (($saved = file_put_contents($filename, serialize($data))) === false) {
+            $this->stderr("Cannot save serialized data to file {$filename}\n", Console::FG_RED);
+            return Controller::EXIT_CODE_ERROR;
+        }
+
+        $this->stdout("Data parsed and saved to import file {$filename}\n", Console::BOLD);
+
+        if (strtoupper($this->runFixture) === 'YES') {
+            $this->stdout("Running fixture load command\n", Console::BOLD);
+            Yii::setAlias('@import-data-dir', Yii::getAlias('@app/commands/data-files'));
+            $run_fixture = \Yii::$app->runAction('fixture/load', ['Application', 'namespace' => 'app\tests\importFixtures']);
+        }
+
+        return Controller::EXIT_CODE_NORMAL;
+    }
+
+    /**
+     * Parse and generate serialized data file with the available choices data included in a CSV file.
+     *
+     * @param string $file the filename of the CSV containing the applicant choices
+     */
+    public function actionParseChoices($file)
+    {
+        $this->stdout("Initiating data parse and import file generation...\n", Console::BOLD);
+
+        $data = $this->getChoicesData($file);
+        if (!is_array($data)) {
+            return $data;
+        }
+        $data['prefecture'] = $this->getPrefecturesData();
+
+        $filename = $this->getOutputFilename($file);
+        if (($saved = file_put_contents($filename, serialize($data))) === false) {
+            $this->stderr("Cannot save serialized data to file {$filename}\n", Console::FG_RED);
+            return Controller::EXIT_CODE_ERROR;
+        }
+
+        $this->stdout("Data parsed and saved to import file {$filename}\n", Console::BOLD);
+
+        if (strtoupper($this->runFixture) === 'YES') {
+            $this->stdout("Running fixture load command\n", Console::BOLD);
+            Yii::setAlias('@import-data-dir', Yii::getAlias('@app/commands/data-files'));
+            $run_fixture = \Yii::$app->runAction('fixture/load', ['Application', 'namespace' => 'app\tests\importFixtures']);
+        }
+
+        return Controller::EXIT_CODE_NORMAL;
+    }
+
+    private function getOutputFilename($file)
+    {
+        return dirname($file) . '/data.serialized.txt';
+    }
+
+    private function getPrefecturesData()
+    {
+        $common_ref = \Yii::$app->crypt->encrypt(json_encode(['sid' => -1]));
+        return [
+            ['id' => 1, 'region' => 'ΚΡΗΤΗΣ', 'prefecture' => 'ΗΡΑΚΛΕΙΟΥ', 'reference' => $common_ref],
+            ['id' => 2, 'region' => 'ΚΡΗΤΗΣ', 'prefecture' => 'ΛΑΣΙΘΙΟΥ', 'reference' => $common_ref],
+            ['id' => 3, 'region' => 'ΚΡΗΤΗΣ', 'prefecture' => 'ΡΕΘΥΜΝΟΥ', 'reference' => $common_ref],
+            ['id' => 4, 'region' => 'ΚΡΗΤΗΣ', 'prefecture' => 'ΧΑΝΙΩΝ', 'reference' => $common_ref],
+        ];
+    }
+
+    /**
+     * Returns APPLICANT, PREFECTURE_PREFERENCE and SPECIALTY filled
+     */
+    private function getApplicantData($file)
+    {
         $csv = Reader::createFromPath($file, 'r')
             ->setDelimiter($this->csvDelimiter)
             ->setEnclosure($this->csvEnclosure);
-        //            ->setHeaderOffset($this->csvSkipLines - 1);
 
         $retrieve_statement = (new Statement())
             ->offset($this->csvSkipLines);
 
-        $fields = explode(',', $this->fields);
-        if (count($fields) !== count($this->_field_mapping)) {
+        $fields = explode(',', $this->fields_applicants);
+        if (count($fields) !== count($this->_field_mapping['applicants'])) {
             $this->stderr('Field count does not match', Console::FG_RED);
             return Controller::EXIT_CODE_ERROR;
         }
 
         $fields_idx = array_combine($fields, $fields);
-        $common_ref = \Yii::$app->crypt->encrypt(json_encode(['sid' => -1]));
         $data = [
             'applicant' => [],
             'specialty' => ['ΕΒΠ'],
-            'prefecture' => [
-                ['id' => 1, 'region' => 'ΚΡΗΤΗΣ', 'prefecture' => 'ΗΡΑΚΛΕΙΟΥ', 'reference' => $common_ref],
-                ['id' => 2, 'region' => 'ΚΡΗΤΗΣ', 'prefecture' => 'ΛΑΣΙΘΙΟΥ', 'reference' => $common_ref],
-                ['id' => 3, 'region' => 'ΚΡΗΤΗΣ', 'prefecture' => 'ΡΕΘΥΜΝΟΥ', 'reference' => $common_ref],
-                ['id' => 4, 'region' => 'ΚΡΗΤΗΣ', 'prefecture' => 'ΧΑΝΙΩΝ', 'reference' => $common_ref],
-            ],
+            'prefecture' => [],
+            'choices' => [],
             'prefecture_preference' => []
         ];
 
         $aid = 100;
         foreach ($retrieve_statement->process($csv) as $record) {
             $aid++;
-            $mapped_record = array_combine($this->_field_mapping, array_intersect_key($record, $fields_idx));
+            $mapped_record = array_combine($this->_field_mapping['applicants'], array_intersect_key($record, $fields_idx));
             //            echo print_r($record, true), PHP_EOL;
             //            echo print_r($mapped_record, true), PHP_EOL;
             $applicant_data_entry = [
                 'id' => $aid, // keep the id
                 'vat' => $mapped_record['vat'],
-                'identity' => $mapped_record['identity'],
+                'identity' => preg_replace('/\s+/', '', $mapped_record['identity']),
                 'specialty' => 'ΕΒΠ',
                 'reference' => \Yii::$app->crypt->encrypt(
                     json_encode([
@@ -159,20 +303,78 @@ class ImportController extends Controller
                 ];
             }
         }
-        $filename = dirname($file) . '/data.serialized.txt';
-        if (($saved = file_put_contents($filename, serialize($data))) === false) {
-            $this->stderr("Cannot save serialized data to file {$filename}\n", Console::FG_RED);
+
+        return $data;
+    }
+
+    /**
+     * Returns CHOICES and SPECIALTY filled
+     */
+    private function getChoicesData($file)
+    {
+        $csv = Reader::createFromPath($file, 'r')
+            ->setDelimiter($this->csvDelimiter)
+            ->setEnclosure($this->csvEnclosure);
+
+        $retrieve_statement = (new Statement())
+            ->offset($this->csvSkipLines);
+
+        $fields = explode(',', $this->fields_choices);
+        if (count($fields) !== count($this->_field_mapping['choices'])) {
+            $this->stderr('Field count does not match', Console::FG_RED);
             return Controller::EXIT_CODE_ERROR;
         }
 
-        $this->stdout("Data parsed and saved to import file {$filename}\n", Console::BOLD);
+        $fields_idx = array_combine($fields, $fields);
+        $common_ref = \Yii::$app->crypt->encrypt(json_encode(['sid' => -1]));
 
-        if (strtoupper($this->runFixture) === 'YES') {
-            $this->stdout("Running fixture load command\n", Console::BOLD);
-            Yii::setAlias('@import-data-dir', Yii::getAlias('@app/commands/data-files'));
-            $run_fixture = \Yii::$app->runAction('fixture/load', ['Application', 'namespace' => 'app\tests\importFixtures']);
+        $data = [
+            'applicant' => [],
+            'specialty' => [],
+            'prefecture' => [],
+            'choices' => [],
+            'prefecture_preference' => []
+        ];
+
+        $aid = 1;
+        foreach ($retrieve_statement->process($csv) as $record) {
+            $aid++;
+            $mapped_record = array_combine($this->_field_mapping['choices'], array_intersect_key($record, $fields_idx));
+//            echo print_r($record, true), PHP_EOL;
+//            echo print_r($mapped_record, true), PHP_EOL;
+
+            $pref = trim($mapped_record['prefecture']);
+            switch (strtoupper($pref)) {
+                case 'Η':
+                    $prefect_id = 1;
+                    break;
+                case 'Λ':
+                    $prefect_id = 2;
+                    break;
+                case 'Ρ':
+                    $prefect_id = 3;
+                    break;
+                case 'Χ':
+                    $prefect_id = 4;
+                    break;
+                default:
+                    $this->stderr("UNKNOWN PREFECTURE IDENTIFIER [{$pref}] [{$mapped_record['preferences']}]", Console::FG_RED);
+                    return Controller::EXIT_CODE_ERROR;
+            }
+            $applicant_data_entry = [
+                'id' => $aid,
+                'specialty' => $mapped_record['specialty'],
+                'count' => $mapped_record['count'],
+                'position' => $mapped_record['position'],
+                'reference' => $common_ref,
+                'prefecture_id' => $prefect_id
+            ];
+
+            $data['choices'][] = $applicant_data_entry;
+            $data['specialty'][] = $mapped_record['specialty'];
         }
+        $data['specialty'] = array_unique($data['specialty']);
 
-        return Controller::EXIT_CODE_NORMAL;
+        return $data;
     }
 }
--- tests/importFixtures/ChoiceFixture.php
+++ tests/importFixtures/ChoiceFixture.php
@@ -1,8 +1,6 @@
 <?php
 namespace app\tests\importFixtures;
 
-use Yii;
-
 class ChoiceFixture extends BaseImportFixture
 {
     public $tableName = '{{%choice}}';
@@ -12,25 +10,7 @@ class ChoiceFixture extends BaseImportFixture
 
     protected function getData()
     {
-        $common_ref = Yii::$app->crypt->encrypt(
-            json_encode([
-            'sid' => -1,
-            ])
-        );
-
-        return [
-            ['specialty' => 'ΕΒΠ', 'count' => 1, 'prefecture_id' => 1, 'position' => 'Σχολείο #1 και Σχολείο #2', 'reference' => $common_ref],
-            ['specialty' => 'ΕΒΠ', 'count' => 2, 'prefecture_id' => 1, 'position' => 'Σχολείο #3', 'reference' => $common_ref],
-            ['specialty' => 'ΕΒΠ', 'count' => 2, 'prefecture_id' => 1, 'position' => 'Σχολείο #2', 'reference' => $common_ref],
-            ['specialty' => 'ΕΒΠ', 'count' => 1, 'prefecture_id' => 1, 'position' => 'Σχολείο #1', 'reference' => $common_ref],
-            ['specialty' => 'ΕΒΠ', 'count' => 2, 'prefecture_id' => 2, 'position' => 'Σχολείο #2.1', 'reference' => $common_ref],
-            ['specialty' => 'ΕΒΠ', 'count' => 2, 'prefecture_id' => 2, 'position' => 'Σχολείο #2.2', 'reference' => $common_ref],
-            ['specialty' => 'ΕΒΠ', 'count' => 1, 'prefecture_id' => 3, 'position' => 'Σχολείο #11 και Σχολείο #12', 'reference' => $common_ref],
-            ['specialty' => 'ΕΒΠ', 'count' => 2, 'prefecture_id' => 3, 'position' => 'Σχολείο #13', 'reference' => $common_ref],
-            ['specialty' => 'ΕΒΠ', 'count' => 2, 'prefecture_id' => 3, 'position' => 'Σχολείο #12', 'reference' => $common_ref],
-            ['specialty' => 'ΕΒΠ', 'count' => 1, 'prefecture_id' => 3, 'position' => 'Σχολείο #11', 'reference' => $common_ref],
-            ['specialty' => 'ΕΒΠ', 'count' => 2, 'prefecture_id' => 4, 'position' => 'Σχολείο #22.1', 'reference' => $common_ref],
-            ['specialty' => 'ΕΒΠ', 'count' => 2, 'prefecture_id' => 4, 'position' => 'Σχολείο #22.2', 'reference' => $common_ref],
-        ];
+        $data = $this->getSerializedData();
+        return $data['choices'];
     }
 }
