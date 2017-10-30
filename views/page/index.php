2b978dd8c4dea8719652ee17bb0af70c35799d95
Stavros Papadakis <spapad@gmail.com>
2017-10-30 12:34:45 +0200

    Add pages for static content

---
+++ views/page/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\PageSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Κείμενα';
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+<div class="page-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Νέο κείμενο', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?=
+    GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            'identity',
+            'title',
+            'created_at',
+            'updated_at',
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]);
+
+    ?>
+</div>
