b0de76d0a8c4ede2e77d0e4750e8e118756544e1
Stavros Papadakis <spapad@gmail.com>
2017-10-30 09:32:46 +0200

    Changes to denial screen (#51)

--- views/application/show-terms.php
+++ views/application/show-terms.php
@@ -4,20 +4,20 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 
-$this->title = 'Επιβεβαίωση δήλωσης άρνησης αίτησης';
+$this->title = 'Επιβεβαίωση ενημέρωσης για τη διαδικασία υποβολής αιτήσεων';
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 <div class="jumbotron">
-    <h1>Επιβεβαίωση αποδοχής όρων</h1>
-    <p>Παρακαλούμε μελετήστε προσεκτικά τις παρακάτω οδηγίες και όρους χρήσης.<p>
-    <p><strong>Για να συνεχίσετε πρέπει να τις αποδεχτείτε.</strong><p>
+    <h2>Επιβεβαίωση ενημέρωσης για τη διαδικασία υποβολής αιτήσεων</h2>
+    <p>Παρακαλούμε μελετήστε προσεκτικά τις παρακάτω οδηγίες.</p>
+    <p><strong>Για να συνεχίσετε πρέπει να τις αποδεχτείτε.</strong></p>
 </div>
 <div class="well">
-    <?= $this->render('_terms') ?>
+    <?= $this->render('_info_terms') ?>
 </div>
 <div class="jumbotron">
     <p>Πατήστε το κουμπί αποδέχομαι εφόσον μελετήσατε και συμφωνείτε με τα παραπάνω.<p>
     <p><?= Html::a('Αποδέχομαι', ['terms-agree'], ['class' => 'btn btn-success', 'data-method' => 'POST']) ?></p>
-    <p><?= Html::a('Άκυρο', ['/site/logout'], ['class' => 'btn btn-danger', 'data-method' => 'POST']) ?></p>
+    <p><?= Html::a('Δεν αποδέχομαι', ['/site/logout'], ['class' => 'btn btn-danger', 'data-method' => 'POST']) ?></p>
 </div>
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -89,8 +89,6 @@ AppAsset::register($this);
         <footer class="footer">
             <div class="container">
                 <p class="pull-left">&copy; <?= Yii::$app->params['companyName'] ?> <?= date('Y') ?></p>
-
-                <p class="pull-right"><?= Yii::powered() ?></p>
             </div>
         </footer>
 
